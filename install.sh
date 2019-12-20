6667578cd13d473ca35c60486542cbbf2c7c2c1e
Antti Suoninen <antti.suoninen@zooplus.com>
2019-12-20 19:22:50 +0100

    conditional stuffs

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 set -ev
 
+if [ ! brew -v ]; then
+	echo 'no has brew'
+fi
+
 curl -o ~/.vimrc https://raw.githubusercontent.com/aumbadgah/configs/master/.vimrc
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/aumbadgah/configs/master/.tmux.conf
 curl -o ~/.zshrc https://raw.githubusercontent.com/aumbadgah/configs/master/.zshrc
