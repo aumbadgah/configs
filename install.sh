83b6ac152761e7ae8dc8fec952cabc7627234b22
Antti Suoninen <antti.suoninen@zooplus.com>
2019-12-20 17:04:42 +0100

    include zshrc

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -ev
 
 curl -o ~/.vimrc https://raw.githubusercontent.com/aumbadgah/configs/master/.vimrc
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/aumbadgah/configs/master/.tmux.conf
+curl -o ~/.zshrc https://raw.githubusercontent.com/aumbadgah/configs/master/.zshrc
 
 tmux source-file ~/.tmux.conf
 
--- update.host-to-repo.sh
+++ update.host-to-repo.sh
@@ -4,4 +4,5 @@ set -ev
 
 cp ~/.vimrc .
 cp ~/.tmux.conf .
+cp ~/.zshrc .
 
